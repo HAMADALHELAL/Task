7008f5080672e3c914d67167725632c3058c3ca6
HAMADALHELAL <wldalhelal@hotmail.com>
2024-01-09 23:58:45 +0300

    gggg

--- taskmultimedia/lib/main.dart
+++ taskmultimedia/lib/main.dart
@@ -1,18 +1,18 @@
+import 'package:taskmultimedia/app/config/routes/routes.dart';
+import 'package:taskmultimedia/app/config/theme/my_theme.dart';
+import 'package:taskmultimedia/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:taskmultimedia/firebase_options.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() async {
-  /// ensures that the framework is ready to handle
-  /// widget-related operations and provides access
-  /// to platform-specific resources
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
-//
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -20,27 +20,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-    );
+    return MaterialApp.router(
+        title: 'Flutter Demo',
+        routerConfig: AppRouter.router,
+        theme: MyTheme.lightTheme,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        locale: const Locale('en'));
   }
 }
